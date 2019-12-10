1cbeffe860c795e4c3e88bf059290e0f4bf5c134
lastnamehurt <clhurt1@gmail.com>
2019-12-09 21:19:22 -0800

    adds follow users function

--- services.py
+++ services.py
@@ -20,9 +20,7 @@ class TweetService:
         self.tweets_cache = {}
         self.users_cache = {}
         self.dont_engage_list = self.api.get_list(owner_screen_name='sole_hunt', slug='sneaker-friends')
-
         self._get_ids_for_users_followed()
-        # self.store_users(self.ids_cache)
 
     def _get_ids_for_users_followed(self, ids=None):
         if not ids or self.ids_cache:
@@ -34,7 +32,10 @@ class TweetService:
 
     def _store_user(self, user_id=None):
         user = self.api.get_user(user_id)
+        if user.id in self.users_cache.keys():
+            return
         self.users_cache[user_id] = user
+        logging.info("Stored user {}".format(user.screen_name))
 
     def store_users(self, ids):
         for user_id in ids:
@@ -68,6 +69,9 @@ class TweetService:
         while rate_limit_ok(rate_limit):
             logging.info("rate limit ok: {}".format(rate_limit_ok(rate_limit)))
             for _user in users:
+                if not self.should_engage(_user):
+                    logging.info("Not going to engage with {}".format(_user))
+                    return
                 user = self.users_cache[_user.id]
                 logging.info("Collecting user {}".format(user.screen_name))
                 if not user.status.favorited:
@@ -120,17 +124,16 @@ class TweetService:
             # create cache if it does not exist
             self.get_relevant_tweets_from_followed()
         for tweet_id, tweet in self.tweets_cache.iteritems():
-            if self.should_engage(tweet['status']):
-                if tweet['status'].entities['urls']:
-                    url = tweet['status'].entities['urls'][0]['url']
-                    logging.info("Engaging tweet {}".format(url))
-                self._engage_tweet(tweet)
-                limit -= ONE
-                time.sleep(ONE)
-                if limit <= ZERO:
-                    limit = TEN
-                    time.sleep(MIN_INTERVAL * 2)
-                    continue
+            if tweet['status'].entities['urls']:
+                url = tweet['status'].entities['urls'][0]['url']
+                logging.info("Engaging tweet {}".format(url))
+            self._engage_tweet(tweet)
+            limit -= ONE
+            time.sleep(ONE)
+            if limit <= ZERO:
+                limit = TEN
+                time.sleep(MIN_INTERVAL * 2)
+                continue
 
     def _reset(self):
         self.ids_cache = []
@@ -138,35 +141,46 @@ class TweetService:
         self.users_cache = {}
         self._get_ids_for_users_followed()
 
-    def should_engage(self, tweet):
+    def should_engage(self, user):
         member_screen_names = [member.screen_name for member in self.dont_engage_list.members()]
-        if tweet.user.screen_name in member_screen_names:
+        if user.screen_name in member_screen_names:
             return False
         return True
 
-    def follow_retweeter(self, tweet):
+    def follow_retweeter(self, user_id):
         """
         follow users that retweet
         :param tweet:
         :return:
         """
+        user = self.users_cache[user_id]
+        tweet = user.status
+
         is_retweeted = tweet.retweeted
         if is_retweeted:
             retweets = tweet.retweets()
             if len(retweets) > 16:
-                retweets = retweets[:17]
+                retweets = retweets[:15]
             for retweet in retweets:
                 # follow user and add to list
                 user = retweet.user
-                if not user.following:
+                if not user.following and user.id != self.api.me().id:
                     user.follow()
-                self.api.add_list_member(NON_ENGAGED_FRIENDS_LIST, user.id)
+                    logging.info("Followed {}".format(user.screen_name))
+                self.api.add_list_member(slug=NON_ENGAGED_FRIENDS_LIST, user_id=user.id, owner_screen_name='sole_hunt')
                 logging.info("Added {} to non engaged list".format(user.screen_name))
 
     def follow_retweeters(self):
+        # first ensure users have been stored
+        self.store_users(self.ids_cache)
+
         followed = []
-        for tweet in self.tweets_cache:
-            self.follow_retweeter(tweet)
+        for user_id, user in self.users_cache.iteritems():
+            try:
+                self.follow_retweeter(user.id)
+                followed.append(user_id)
+            except:
+                continue
         logging.info("Followed users: {}".format(followed))
         return followed
 
